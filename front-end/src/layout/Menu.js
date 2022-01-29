17ab92ef67d66c90eab9af0e90119de8748d01a6
Ayman Abusaif <aabusaif2001@gmail.com>
2022-01-28 19:33:24 -0600

    update

--- front-end/src/layout/Menu.js
+++ front-end/src/layout/Menu.js
@@ -10,46 +10,46 @@ import { Link } from "react-router-dom";
 
 function Menu() {
   return (
-    <nav className="navbar navbar-dark align-items-start p-0">
-      <div className="container-fluid d-flex flex-column p-0">
+    <nav className="menu navbar navbar-dark align-items-start p-0">
+      <div className="menu container-fluid d-flex flex-column p-0">
         <Link
           className="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-0"
           to="/"
         >
-          <div className="sidebar-brand-text mx-3">
-            <span>Periodic Tables</span>
+          <div className="menu sidebar-brand-text mx-3">
+            <span className="menu">Periodic Tables</span>
           </div>
         </Link>
-        <hr className="sidebar-divider my-0" />
-        <ul className="nav navbar-nav text-light" id="accordionSidebar">
-          <li className="nav-item">
+        <hr className="menu sidebar-divider my-0" />
+        <ul className="menu nav navbar-nav text-light" id="accordionSidebar">
+          <li className="menu nav-item">
             <Link className="nav-link" to="/dashboard">
-              <span className="oi oi-dashboard" />
+              <span className="menu oi oi-dashboard" />
               &nbsp;Dashboard
             </Link>
           </li>
-          <li className="nav-item">
+          <li className="menu nav-item">
             <Link className="nav-link" to="/search">
-              <span className="oi oi-magnifying-glass" />
+              <span className="menu oi oi-magnifying-glass" />
               &nbsp;Search
             </Link>
           </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/reservations/new">
-              <span className="oi oi-plus" />
+          <li className="menu nav-item">
+            <Link className="menu nav-link" to="/reservations/new">
+              <span className="menu oi oi-plus" />
               &nbsp;New Reservation
             </Link>
           </li>
-          <li className="nav-item">
+          <li className="menu nav-item">
             <Link className="nav-link" to="/tables/new">
-              <span className="oi oi-layers" />
+              <span className="menu oi oi-layers" />
               &nbsp;New Table
             </Link>
           </li>
         </ul>
-        <div className="text-center d-none d-md-inline">
+        <div className="menu text-center d-none d-md-inline">
           <button
-            className="btn rounded-circle border-0"
+            className="menu btn rounded-circle border-0"
             id="sidebarToggle"
             type="button"
           />
